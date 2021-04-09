09f90dd736ed885409ca96f356359e6a51a73b2d
Victor Saad <22480813+victorsaad00@users.noreply.github.com>
2021-04-09 15:26:28 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "username": "root",
-  "password": "135246",
-  "database": "instagram",
+  "username": "",
+  "password": "",
+  "database": "",
   "host": "127.0.0.1",
   "dialect": "mysql"
 }
